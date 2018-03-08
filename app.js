77fd2a7c5cc2cf9753df1aa30f5361ab76424591
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-08 12:08:22 +0000

    require CLIENT_ORIGIN for deployment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const { CLIENT_ORIGIN } = require('./config');
 const index = require('./routes/index');
 const users = require('./routes/users');
 
