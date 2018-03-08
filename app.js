004fc34395e9e6374679ec776eeb4ac489b67662
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-08 11:59:05 +0000

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -24,6 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
