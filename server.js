0599ec002fa4c43453236c453ed202d415f7a4cb
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-12 10:56:53 +0000

    remove server.js - revert to app.js-only setup

--- server.js
+++
@@ -1,6 +0,0 @@
-const { app, runServer, closeServer } = require('./app');
-const { PORT } = require('./config');
-
-app.listen(PORT, () => {
-  console.log(`The app is listening on port ${PORT}`);
-});
