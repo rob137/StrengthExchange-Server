2cf51c577d9aef19e245294b35d089e7ece6dc56
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-21 12:03:45 +0000

    remove "exports."

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     'mongodb://localhost/strength-app',
-  exports.TEST_DATABASE_URL =
+  TEST_DATABASE_URL =
     process.env.TEST_DATABASE_URL ||
     global.DATABASE_URL ||
     'mongodb://localhost/test-strength-app';
