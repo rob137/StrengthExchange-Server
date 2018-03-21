abaa67a8bd7f3dc74ab45f1d42c979df48a03823
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-21 12:04:46 +0000

    remove semicolon

--- config.js
+++ config.js
@@ -10,6 +10,6 @@ module.exports = {
   TEST_DATABASE_URL =
     process.env.TEST_DATABASE_URL ||
     global.DATABASE_URL ||
-    'mongodb://localhost/test-strength-app';
+    'mongodb://localhost/test-strength-app',
   PORT: process.env.PORT || 5002,
 };
