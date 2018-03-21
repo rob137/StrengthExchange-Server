a2f4d55345f11a23f4b11a0ba3305b3cd78f110d
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-21 11:59:07 +0000

    add test db url

--- config.js
+++ config.js
@@ -7,5 +7,9 @@ module.exports = {
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     'mongodb://localhost/strength-app',
+  exports.TEST_DATABASE_URL =
+    process.env.TEST_DATABASE_URL ||
+    global.DATABASE_URL ||
+    'mongodb://localhost/test-strength-app';
   PORT: process.env.PORT || 5002,
 };
